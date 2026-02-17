cc68a167addbf98283809286207a70d127be97a6
Eirik Vaageskar <eirik.vageskar@sparebank1.no>
2026-02-17 10:13:56 +0100

    Sett modernTargets til ~95% dekning for norske brukere
    Legger til Samsung Internet, Opera og Opera Mobile som
    manglende nettlesere i browserslist-queryen.
    
    Denne query-en ble funnet med
    `npx browserslist --coverage=NO "cover 95% in NO"`
    
    Polyfill-bundlen mer enn dobles i størrelse:
    
    ```
    $ npm run build
    
    > vite-build-target-does-not-polyfill-mwe@0.0.0 build
    > tsc && vite build
    
    vite v7.3.1 building client environment for production...
    ✓ 7 modules transformed.
    dist/index.html                     0.57 kB │ gzip:  0.33 kB
    dist/assets/index-BmeGit54.css      1.20 kB │ gzip:  0.62 kB
    dist/assets/index-x87qYTLl.js       3.22 kB │ gzip:  1.68 kB
    dist/assets/polyfills-C9-YE7xf.js  32.93 kB │ gzip: 13.09 kB
    ✓ built in 2.52s
    ```

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import legacy from "@vitejs/plugin-legacy";
 export default {
   plugins: [
     legacy({
-      modernTargets: ["edge>=88", "firefox>=78", "chrome>=87", "safari>=14"],
+      modernTargets: [
+        "edge>=79",
+        "firefox>=67",
+        "chrome>=64",
+        "safari>=11",
+        "chromeAndroid>=64",
+        "iOS>=11",
+        "samsung>=4",
+        "opera>=50",
+        "op_mob>=50",
+      ],
       modernPolyfills: true,
       renderLegacyChunks: false,
     }),
